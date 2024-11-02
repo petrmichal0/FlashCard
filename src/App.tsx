f21dc51dbd4fa9a9e6194875a5a51de95368a1d9
Petr Michal <petrmichal0@gmail.com>
2024-11-02 20:59:33 +0100

    add framer motion effects #3

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
 import { useState } from "react";
 import { questions, Question } from "./data";
+import { motion } from "framer-motion";
+
+const animationAllCards = (index: number) => ({
+  initial: { opacity: 0, x: -50 },
+  animate: { opacity: 1, x: 0 },
+  transition: { duration: 0.5, delay: index * 0.1 },
+});
+
+const animationCard = (isSelected: boolean) => ({
+  initial: { opacity: 0 },
+  animate: { opacity: 1, scale: isSelected ? 1.5 : 1 },
+  transition: { duration: 0.3 },
+});
 
 function App() {
   const [selectedId, setSelectedId] = useState<number | null>(null);
@@ -10,18 +23,19 @@ function App() {
 
   return (
     <div className="flashcards">
-      {questions.map(({ id, question, answer }: Question) => {
+      {questions.map(({ id, question, answer }: Question, index) => {
         const isSelected = id === selectedId;
         const content = isSelected ? answer : question;
 
         return (
-          <div
+          <motion.div
             key={id}
             onClick={() => handleClick(id)}
             className={`flashcards__item ${isSelected ? "selected" : ""}`}
+            {...animationAllCards(index)}
           >
-            {content}
-          </div>
+            <motion.div {...animationCard(isSelected)}>{content}</motion.div>
+          </motion.div>
         );
       })}
     </div>
